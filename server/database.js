83ce78640a0256848ffc38e7289ac84db3abd343
Nurulla Ibadov <ibadnurulla@gmail.com>
2026-02-11 18:45:22 +0400

    Addedddddd

--- server/database.js
+++ server/database.js
@@ -1,10 +1,35 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
+// const { Sequelize } = require('sequelize');
+// const path = require('path');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'database.sqlite'),
-  logging: false
-});
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: path.join(__dirname, 'database.sqlite'),
+//   logging: false
+// });
+
+// module.exports = sequelize;
+
+
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  })
+  : new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: false
+  });
 
 module.exports = sequelize;
+
+
